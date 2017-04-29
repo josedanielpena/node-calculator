04918928f663965fcb0ad865a360d19b1e9d4a27
Ivan Hincapie <ivanhincapie@gmail.com>
2017-04-29 18:36:17 -0500

    Se agrego la funcion de multiplicacion y division

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 //CALCULADORA
 
+const { multiplicacion, division } = require('./operations');
+
+console.log (multiplicacion(3,5));
+console.log (division(6,3));
--- operations/index.js
+++ operations/index.js
@@ -1,3 +1,11 @@
 //LISTADO DE OPREACIONES
 
 const operation = require('./operation/addition');
+const division = require('./operation/division');
+const multiplicacion = require('./operation/multiplicacion');
+
+module.exports = {
+    division: division,
+    multiplicacion: multiplicacion,
+};
+
